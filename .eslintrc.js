d30d45a5f8ea521fbed6fc163b3e096a1841b72a
Tarang Hirani <tarang9211@gmail.com>
2017-04-20 00:28:57 +0530

    setup: added linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "env": {
+      "browser": true,
+      "es6": true,
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
